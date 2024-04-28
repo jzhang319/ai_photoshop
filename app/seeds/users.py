2d29b93e835c3eae679ce4f317af76e58fa1f170
jzhang319 <jzhang319@gmail.com>
2024-04-28 14:08:46 -0400

    changed seeds, updated favicon and title

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,16 +4,13 @@ from sqlalchemy.sql import text
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    jackz = User(
+        username='jackz', email='jackz@aa.io', password='password', profile_url='https://i.imgur.com/2YVtZzv.jpg')
+    sagehenny = User(
+        username='sagehenny', email='sagehenny@aa.io', password='password', profile_url='https://i.imgur.com/2YVtZzv.jpg')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(jackz)
+    db.session.add(sagehenny)
     db.session.commit()
 
 
@@ -28,5 +25,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
